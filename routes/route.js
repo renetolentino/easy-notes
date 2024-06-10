cb8bccd94dd5dac71412ca0772fc33947d2a067a
Renê Tolentino <rene.tolentino1@hotmail.com>
2024-06-09 22:08:28 -0300

    trying to fix the render

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('../app/buid/');
+  res.render('app/build');
 });
 
 module.exports = router;
